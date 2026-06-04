48cd950cf2a7b2591a94fab0cda6b54ddeb6ba87
Manishasahu19 <manishakumari91492@gmail.com>
2026-06-05 05:06:20 +0530

    Update settings.py

--- office_emp_proj/office_emp_proj/settings.py
+++ office_emp_proj/office_emp_proj/settings.py
@@ -125,5 +125,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL='index'
-
-    CSRF_TRUSTED_ORIGINS = ['https://ems-pro-o6nm.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://ems-pro-o6nm.onrender.com']
