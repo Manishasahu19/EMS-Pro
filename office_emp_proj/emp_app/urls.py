450ed7de5486ec360c82e2dd3f52b0f49babd3dc
Manishasahu19 <manishakumari91492@gmail.com>
2026-06-05 03:31:53 +0530

    Add files via upload

---
+++ office_emp_proj/emp_app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    #login and logout
+    path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+     path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
+
+    path('', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('remove_emp/<int:emp_id>', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+]
