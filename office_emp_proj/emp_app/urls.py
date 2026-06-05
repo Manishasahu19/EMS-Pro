356f2ef1a9f5bbe0241671bd9d49e80a24ca44a2
Manishasahu19 <manishakumari91492@gmail.com>
2026-06-05 10:55:03 +0530

    Update urls.py

--- office_emp_proj/emp_app/urls.py
+++ office_emp_proj/emp_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
      path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
 
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
     path('all_emp', views.all_emp, name='all_emp'),
     path('add_emp', views.add_emp, name='add_emp'),
     path('remove_emp', views.remove_emp, name='remove_emp'),
