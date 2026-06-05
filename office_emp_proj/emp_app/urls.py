7c83e253e4f50b91b973cb920a887b29caee13d5
Manishasahu19 <manishakumari91492@gmail.com>
2026-06-05 11:25:13 +0530

    Update urls.py

--- office_emp_proj/emp_app/urls.py
+++ office_emp_proj/emp_app/urls.py
@@ -3,14 +3,14 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-
+   ा
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-
+  
     path('home/', views.index, name='index'),
     
-    
+  
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('all_emp', views.all_emp, name='all_emp'),
     path('add_emp', views.add_emp, name='add_emp'),
     path('remove_emp', views.remove_emp, name='remove_emp'),
