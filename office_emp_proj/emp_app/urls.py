03a90f4637996cba43aff0f533d38df608c08b3e
Manishasahu19 <manishakumari91492@gmail.com>
2026-06-05 11:28:04 +0530

    Update urls.py

--- office_emp_proj/emp_app/urls.py
+++ office_emp_proj/emp_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-   ा
     path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     
   
